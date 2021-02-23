ed0062e38248c69fae10a5c0fea6a445cdbe867f
Alexis Dietl <alexisdietl@gmail.com>
2021-02-23 20:03:35 -0300

    adding programatic navigation

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  styleUrls: ['./recipe-edit.component.css'],
 })
 export class RecipeEditComponent implements OnInit {
-
-  constructor() { }
+  id: number;
+  editMode = false;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((param: Params) => {
+      this.id = +param['id'];
+      this.editMode = param['id'] != null;
+    });
   }
-
 }
